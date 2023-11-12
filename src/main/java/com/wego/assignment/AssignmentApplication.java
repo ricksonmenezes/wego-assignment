aae590712eb5f59fe7883e26981f921c4fc76295
Rickson <rmenezes@hifinite.com>
2023-11-12 18:15:08 +0530

    removed exclude. had added initially just to stop it complaining when bringing up docker for the first time

--- src/main/java/com/wego/assignment/AssignmentApplication.java
+++ src/main/java/com/wego/assignment/AssignmentApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class AssignmentApplication {
 
